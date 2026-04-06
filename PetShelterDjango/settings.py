a24a712e0622f335792fd48ac5bd3108547cd816
Alexander Kostadinov <149735725+apkostadinov@users.noreply.github.com>
2026-04-06 22:43:01 +0300

    chore: moving media to static folder.

--- PetShelterDjango/settings.py
+++ PetShelterDjango/settings.py
@@ -181,14 +181,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-# Media
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
-
 # Static
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Media (served from static/media)
+MEDIA_URL = "/static/media/"
+MEDIA_ROOT = BASE_DIR / "static" / "media"
 
 STORAGES = {
     "default": {
